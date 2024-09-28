69f67ffe6e666771274a537b44fcabe933878b09
moritzstoeckl <moritz.stoeckl1997@gmail.com>
2024-09-28 02:58:58 +0200

    feat:use environment variables for database configuration and connection

--- src/main/java/com/quiz_platform/Application.java
+++ src/main/java/com/quiz_platform/Application.java
@@ -1,5 +1,6 @@
 package com.quiz_platform;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
@@ -12,6 +13,23 @@ import org.springframework.core.env.Environment;
 public class Application {
 
 	public static void main(String[] args) {
+		// Load environment variables from .env file
+		Dotenv dotenv = Dotenv.load();
+
+		// Set environment variables as system properties
+		System.setProperty("storingDb.rootPassword", dotenv.get("STORING_DB_ROOT_PASSWORD"));
+		System.setProperty("storingDb.user", dotenv.get("STORING_DB_USER"));
+		System.setProperty("storingDb.password", dotenv.get("STORING_DB_PASSWORD"));
+		System.setProperty("storingDb.name", dotenv.get("STORING_DB_NAME"));
+		System.setProperty("storingDb.port", dotenv.get("STORING_DB_PORT"));
+		System.setProperty("storingDb.domain", dotenv.get("STORING_DB_DOMAIN"));
+
+		System.setProperty("authDb.rootPassword", dotenv.get("AUTH_DB_ROOT_PASSWORD"));
+		System.setProperty("authDb.user", dotenv.get("AUTH_DB_USER"));
+		System.setProperty("authDb.password", dotenv.get("AUTH_DB_PASSWORD"));
+		System.setProperty("authDb.name", dotenv.get("AUTH_DB_NAME"));
+		System.setProperty("authDb.port", dotenv.get("AUTH_DB_PORT"));
+		System.setProperty("authDb.domain", dotenv.get("AUTH_DB_DOMAIN"));
 		SpringApplication.run(Application.class, args);
 	}
 
