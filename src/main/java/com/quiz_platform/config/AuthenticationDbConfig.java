42413aee4845aa3c904a7dd366be4bf5bf3e7888
moritzstoeckl <moritz.stoeckl1997@gmail.com>
2024-10-29 16:46:12 +0100

    docs: Added Javadoc to beans.

--- src/main/java/com/quiz_platform/config/AuthenticationDbConfig.java
+++ src/main/java/com/quiz_platform/config/AuthenticationDbConfig.java
@@ -2,6 +2,8 @@ package com.quiz_platform.config;
 
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,35 +14,52 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(
-        basePackages = "com.quiz_platform.authenticationdb.repository", // Package for authdb repositories
+        basePackages = "com.quiz_platform.authenticationdb.repository",
         entityManagerFactoryRef = "authDbEntityManagerFactory",
         transactionManagerRef = "authDbTransactionManager"
 )
 public class AuthenticationDbConfig {
 
+    /**
+     * Configures the data source for the authentication database using properties prefixed with "authdb.datasource".
+     *
+     * @return the configured DataSource for the authentication database.
+     */
     @Bean(name = "authDbDataSource")
     @ConfigurationProperties(prefix = "authdb.datasource")
     public DataSource authDbDataSource() {
         return DataSourceBuilder.create().build();
     }
 
+    /**
+     * Creates the EntityManagerFactory for the authentication database to handle persistence unit settings.
+     *
+     * @param builder    the EntityManagerFactoryBuilder used for building the factory.
+     * @param dataSource the DataSource for the authentication database.
+     * @return a LocalContainerEntityManagerFactoryBean for the authentication database.
+     */
     @Bean(name = "authDbEntityManagerFactory")
     public LocalContainerEntityManagerFactoryBean authDbEntityManagerFactory(
             EntityManagerFactoryBuilder builder,
             @Qualifier("authDbDataSource") DataSource dataSource) {
         return builder
                 .dataSource(dataSource)
-                .packages("com.quiz_platform.authenticationdb.entity") // Package for authdb entities
+                .packages("com.quiz_platform.authenticationdb.entity")
                 .persistenceUnit("authDbPU")
                 .build();
     }
 
+    /**
+     * Sets up the transaction manager for the authentication database, associating it with the
+     * authentication database EntityManagerFactory.
+     *
+     * @param entityManagerFactory the EntityManagerFactory for the authentication database.
+     * @return the PlatformTransactionManager for managing transactions on the authentication database.
+     */
     @Bean(name = "authDbTransactionManager")
     public PlatformTransactionManager authDbTransactionManager(
             @Qualifier("authDbEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
