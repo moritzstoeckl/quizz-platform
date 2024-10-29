d72132ad41e876210ecfb0de6fe445fa231da659
moritzstoeckl <moritz.stoeckl1997@gmail.com>
2024-10-29 16:52:29 +0100

    feat(db): Create table script because tables are not automatically created with hibernate.

---
+++ schema-authdb.sql
@@ -0,0 +1,18 @@
+-- Create users table
+CREATE TABLE IF NOT EXISTS users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    isDeactivated BOOLEAN NOT NULL
+    );
+
+-- Create sessions table
+CREATE TABLE IF NOT EXISTS sessions (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    jwtToken VARCHAR(255) UNIQUE NOT NULL,
+    user_id BIGINT NOT NULL,
+    createdAt DATETIME NOT NULL,
+    expiresAt DATETIME NOT NULL,
+    isActive BOOLEAN NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+    );
