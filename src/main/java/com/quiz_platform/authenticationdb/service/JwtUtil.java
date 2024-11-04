24d00a2b9b1ff4aac1ea1ca10abda87fa387b06c
moritzstoeckl <moritz.stoeckl1997@gmail.com>
2024-11-04 17:05:52 +0100

    refactor: jwt secret is now retrieved from application.properties.

--- src/main/java/com/quiz_platform/authenticationdb/service/JwtUtil.java
+++ src/main/java/com/quiz_platform/authenticationdb/service/JwtUtil.java
@@ -6,7 +6,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +15,10 @@ import java.util.Date;
 
 @Service
 public class JwtUtil {
-    Dotenv dotenv = Dotenv.load();
-
     private final Algorithm algorithm;
     private final int EXPIRATION_TIME = 2; // Expiration time in hours
 
-    public JwtUtil() {
-        final String secret = dotenv.get("JWT_SECRET");
-
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
         if (secret == null) {
             throw new IllegalArgumentException("No secret exists!");
         }
